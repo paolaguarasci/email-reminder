b41b69922bf7e6ac11f58a8f8edc721144760f02
Paola Guarasci <paolaguarasci@gmail.com>
2018-09-18 10:49:36 +0200

    find client with debts

--- app.js
+++ app.js
@@ -111,31 +111,55 @@ const transporter = nodemailer.createTransport({
         pass: '4Jkkwr2GpPqhVVSw5q'
     }
 });
+//Read data from an Excel spreadsheet.
+const xlsx = require('node-xlsx');
+const fs = require('fs');
+var data = xlsx.parse(__dirname + '/data/duesRecords.xlsx'); // parses a file
+var data = xlsx.parse(fs.readFileSync(__dirname + '/data/duesRecords.xlsx'));
+// console.log(data[0].data)
+let list = clientWithDebts(data[0].data);
+// Find all member who have not paid dues for the latest month.
+function clientWithDebts(data) {
+    let result = []
+    let month = data[0].slice(3).length;
+    console.log(month);
+    let clients = data.slice(1);
+    for (let i in clients) {
+        if (clients[i].slice(3).length != month) {
+            result.push({
+                name: clients[i][0],
+                phone: clients[i][1],
+                email: clients[i][2],
+                month: month - clients[i].length + 3,
+            });
+        }
+    }
+    return result;
+}
 
-
-// Find all members who have not paid dues for the latest month.
+console.log(list);
 
 // Find their email addresses and send them personalized reminders
-let message = {
-    // Comma separated list of recipients
-    to: recipient,
+// let message = {
+//     // Comma separated list of recipients
+//     to: recipient,
 
-    // Subject of the message
-    subject: subject,
+//     // Subject of the message
+//     subject: subject,
 
-    // plaintext body
-    text: mailTxt
-}
-transporter.sendMail(message, (error, info) => {
-    if (error) {
-        console.log('Error occurred');
-        console.log(error.message);
-        return process.exit(1);
-    }
-    console.log('Message sent successfully!');
-    console.log(nodemailer.getTestMessageUrl(info));
-    // only needed when using pooled connections
-    transporter.close();
-});
+//     // plaintext body
+//     text: mailTxt
+// }
+// transporter.sendMail(message, (error, info) => {
+//     if (error) {
+//         console.log('Error occurred');
+//         console.log(error.message);
+//         return process.exit(1);
+//     }
+//     console.log('Message sent successfully!');
+//     console.log(nodemailer.getTestMessageUrl(info));
+//     // only needed when using pooled connections
+//     transporter.close();
+// });
 
 // Find their phone number and send them personalized reminders
